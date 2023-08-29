365f8cbf2485c823d2ccf10de4b1e43fbd2a0852
Rog3lio19 <rogeliochavez48@gmail.com>
2023-08-28 21:37:01 -0700

    prueba

--- server/index.js
+++ server/index.js
@@ -1,47 +1,48 @@
-import express from "express";
-import cors from "cors";
-import alumnosRoutes from "./routes/alumnos.routes.js";
-import familiaresRoutes from "./routes/familiares.routes.js";
-import loginRoutes from "./routes/login.routes.js";
-import cookieParser from "cookie-parser";
-import multer from "multer";
-import { dirname, extname, join } from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
-import { PORT } from "./config.js";
+import express from 'express'
+import cors from 'cors'
+import alumnosRoutes from './routes/alumnos.routes.js'
+import familiaresRoutes from './routes/familiares.routes.js'
+import loginRoutes from './routes/login.routes.js'
+import cookieParser from 'cookie-parser'
+import multer from 'multer'
+import { dirname, extname, join } from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import { PORT } from './config.js'
+import { prisma } from '../db.js'
 
-const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
-const mimetypes = ["image/jpeg", "image/png", "application/pdf"];
+const CURRENT_DIR = dirname(fileURLToPath(import.meta.url))
+const mimetypes = ['image/jpeg', 'image/png', 'application/pdf']
 
 const multerUpload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      const path = `uploads/${req.query.id}`;
-      if (!fs.existsSync(path)) fs.mkdirSync(path);
-      cb(null, path);
+      const path = `uploads/${req.query.id}`
+      if (!fs.existsSync(path)) fs.mkdirSync(path)
+      cb(null, path)
     },
     filename: (req, file, cb) => {
-      const fileExtension = extname(file.originalname);
-      const fileName = file.originalname.split(fileExtension)[0];
+      const fileExtension = extname(file.originalname)
+      const fileName = file.originalname.split(fileExtension)[0]
 
-      cb(null, `${fileName}-${Date.now()}${fileExtension}`);
-    },
+      cb(null, `${fileName}-${Date.now()}${fileExtension}`)
+    }
   }),
   fileFilter: (req, file, cb) => {
-    if (mimetypes.includes(file.mimetype)) cb(null, true);
+    if (mimetypes.includes(file.mimetype)) cb(null, true)
     else
       cb(
         new Error(
-          `Solo este tipo de archivos son permitidos '${mimetypes.join(" ")}' `
+          `Solo este tipo de archivos son permitidos '${mimetypes.join(' ')}' `
         )
-      );
+      )
   },
   limits: {
-    fieldSize: 10000000,
-  },
-});
+    fieldSize: 10000000
+  }
+})
 
-const app = express();
+const app = express()
 
 // const corsOptions = {
 //   //Permite que se guarde la cookie con el jwt
@@ -50,16 +51,22 @@ const app = express();
 //   optionSuccessStatus: 200,
 // };
 
-app.use("/public", express.static(join(CURRENT_DIR, "../uploads/")));
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.get("/", async (req, res) => {
-  res.send("Hola mundo");
-});
-app.use("/api", multerUpload.array("file"), alumnosRoutes);
-app.use("/api", familiaresRoutes);
-app.use("/api", loginRoutes);
+app.use('/public', express.static(join(CURRENT_DIR, '../uploads/')))
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+app.get('/', async (req, res) => {
+  const alumno = await prisma.alumnos.findFirst({
+    where: {
+      id_alumno: parseInt(req.params.id)
+    }
+  })
 
-app.listen(PORT);
-console.log("Server on port: ", PORT);
+  res.send(alumno)
+})
+app.use('/api', multerUpload.array('file'), alumnosRoutes)
+app.use('/api', familiaresRoutes)
+app.use('/api', loginRoutes)
+
+app.listen(PORT)
+console.log('Server on port: ', PORT)
